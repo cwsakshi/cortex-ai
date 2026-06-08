f6c9c63d8ac1cb03979457392f326af4c8b2924b
SAKSHI SINGH <sakshisingh@SAKSHIs-MacBook-Air.local>
2026-06-08 15:49:23 +0530

    initial commit: first LLM call with LangChain

---
+++ main.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+
+load_dotenv()
+
+api_key = os.getenv("GROQ_API_KEY")
+
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    api_key=api_key
+)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a pirate who explains technology concepts."),
+    ("human", "Explain {topic} in simple terms in 3 lines.")
+])
+
+chain = prompt | llm
+
+response = chain.invoke({"topic": "quantum computing"})
+
+print(response.content)
